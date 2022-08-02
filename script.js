76e9b603699d9d1dbd436cf19e44574604a82dbb
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-08-02 18:10:21 -0500

    updated the gallery, and more javascript action

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+// NavBar-SideNav
 let toggle = document.getElementById('toggle')
 let navbarShow = document.getElementById('navbar-list')
-
-// toggle.addEventListener('click', sideNav)
 
 document.onclick = function(e){
     if(e.target.id !== 'navbarShow' && e.target.id !== 'toggle')
@@ -9,9 +8,60 @@ document.onclick = function(e){
         toggle.classList.remove('active')
         navbarShow.classList.remove('active')
     }
-}
+    }
     toggle.onclick = function(){
     toggle.classList.toggle('active')
     navbarShow.classList.toggle('active')
     }
 
+//Cart 
+let cart = document.querySelector('.cart');
+let btnCart = document.querySelector('.btnCart')
+
+btnCart.addEventListener('click', OpenCart);
+
+function OpenCart() {
+    cart.classList.toggle('hidden');
+}
+
+//Counter
+const btnPlus = document.querySelector('#btnPlus')
+const btnMinus = document.querySelector('#btnMinus')
+const productCounter = document.querySelector('.counter')
+
+let productCounterValue = 1;
+
+btnPlus.addEventListener('click', productCounterPlus)
+btnMinus.addEventListener('click', productCounterMinus)
+
+function productCounterPlus() {
+    // console.log(productCounterValue);
+    setProductCounter(1);
+}
+
+function productCounterMinus() {
+    setProductCounter(-1);
+}
+
+function setProductCounter(value){
+    if ((productCounterValue + value) > 0) {
+        productCounterValue += value;
+        productCounter.innerHTML = productCounterValue
+    }
+}
+
+//Gallery
+const gallery = document.querySelectorAll('.pic')
+
+gallery.forEach(img => {
+    img.addEventListener('click', onThumbClick)
+})
+
+function onThumbClick(){
+    //clear active state for all thumbnails
+    gallery.forEach(img=> {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    event.target.parentElement.classList.add('active');
+}
