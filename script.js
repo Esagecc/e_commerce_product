4b3e914afd1d47e0dd8767290d680df933e1e8ea
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-08-06 14:00:01 -0500

    completed the gallery and cart

--- script.js
+++ script.js
@@ -162,9 +162,17 @@ const gallery = document.querySelectorAll('.pic')
 const heroImg = document.querySelector('.product-hero')
 const btnNext = document.querySelector('.next')
 const btnPrevious = document.querySelector('.previous')
+const overlay = document.querySelector('.overlay');
+const lightbox = document.querySelector('.lightbox')
+
+let lightboxGallery;
+let lightboxHero;
+
+
 
 btnNext.addEventListener('click', handleBtnClickNext)
 btnPrevious.addEventListener('click', handleBtnClickPrevious)
+heroImg.addEventListener('click', onHeroImgClick)
 
 gallery.forEach(img => {
     img.addEventListener('click', onThumbClick)
@@ -214,5 +222,72 @@ function setHeroImage(imageIndex) {
 }
 
 function onHeroImgClick() {
-    overlay.classList.remove('hidden');
+    if (window.innerWidth >= 1440) {
+        if (overlay.childElementCount == 1){
+            const newNode = lightbox.cloneNode(true);
+            overlay.appendChild(newNode);
+
+            const btnOverlayClose = document.querySelector('#btnOverlayClose');
+            btnOverlayClose.addEventListener('click', onBtnOverlayClose)
+
+            lightboxGallery = overlay.querySelectorAll('.pic');
+            lightboxHero = overlay.querySelector('.product-hero');
+            lightboxGallery.forEach(img => {
+                img.addEventListener('click', onThumbClickLightbox);
+            });
+            const btnOverlayNext = overlay.querySelector('.next');
+            const btnOverlayPrevious = overlay.querySelector('.previous');
+            btnOverlayNext.addEventListener('click', handleBtnClickNextOverlay); 
+            btnOverlayPrevious.addEventListener('click', handleBtnClickPreviousOverlay); 
+        }
+        overlay.classList.remove('hidden');
+    }
+}
+
+function onBtnOverlayClose() {
+    overlay.classList.add('hidden');
+}
+
+function onThumbClickLightbox(event){
+    //clear active state for all thumbnails
+    lightboxGallery.forEach(img=> {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    event.target.parentElement.classList.add('active');
+    //update hero image
+    lightboxHero.src = event.target.src.replace('-thumbnail', '')
+}
+
+
+function handleBtnClickNextOverlay(){
+    let imageIndex = getOverlayCurrentImageIndex();
+    imageIndex++;
+    if (imageIndex > 4) {
+        imageIndex = 1;
+    }
+    setOverlayHeroImage(imageIndex);
+}
+
+function handleBtnClickPreviousOverlay(){
+    let imageIndex = getOverlayCurrentImageIndex();
+    imageIndex--;
+    if (imageIndex < 1) {
+        imageIndex = 4;
+    }
+    setOverlayHeroImage(imageIndex);
+}
+
+function getOverlayCurrentImageIndex() {
+    const imageIndex = parseInt(lightboxHero.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', ''));
+    return imageIndex;
+}
+
+function setOverlayHeroImage(imageIndex) {
+    lightboxHero.src = `./images/image-product-${imageIndex}.jpg`;
+    //images are not sync
+    lightboxGallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    lightboxGallery[imageIndex-1].classList.add('active');
 }
